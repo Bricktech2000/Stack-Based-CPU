59f2c276eb33889a90c63409c7df8fa7d8324779
Bricktech2000 <bricktech2000@gmail.com>
2021-12-13 22:48:08 -0500

    slight refactor

--- src/bin/asm.rs
+++ src/bin/asm.rs
@@ -5,41 +5,35 @@ fn main() {
   // https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html
   let args: Vec<String> = env::args().collect();
   if args.len() != 2 {
-    println!("Usage: ./asm <filename>");
+    println!("Usage: asm <filename>");
     return;
   }
 
   println!("Assembling Hex...");
 
   // https://stackoverflow.com/questions/31192956/whats-the-de-facto-way-of-reading-and-writing-files-in-rust-1-x
-  let hex_bytes = fs::read_to_string(&args[1]).expect("Unable to read file.");
+  let in_chars = fs::read_to_string(&args[1]).expect("Unable to read file.");
 
   let mut out_bytes: Vec<u8> = vec![];
   let mut accumulator: u8 = 0;
-  let mut first_byte = true;
+  let mut first_nibble = true;
   // https://doc.rust-lang.org/rust-by-example/flow_control/for.html
   // https://doc.rust-lang.org/rust-by-example/types/cast.html
   let hex_chars: Vec<char> = vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-  for hex_char in hex_bytes.chars() {
+  for in_char in in_chars.chars() {
     // https://blog.mgattozzi.dev/how-do-i-str-string/
-    if " \n\t".contains(hex_char) {
-      // ignore whitespace
-      continue;
-    }
-    if hex_chars.contains(&hex_char) {
-      // https://stackoverflow.com/questions/30558246/how-do-i-find-the-index-of-an-element-in-an-array-vector-or-slice
-      let digit = hex_chars.iter().position(|&r| r == hex_char).unwrap() as u8;
-      accumulator <<= 4;
-      accumulator |= digit;
-      first_byte = !first_byte;
+    // ignore whitespace and comments
+    if " \n\t".contains(in_char) { continue; }
+    if !hex_chars.contains(&in_char) { continue; }
 
-      if first_byte {
-        out_bytes.push(accumulator);
-        accumulator = 0;
-      }
-    } else {
-      // probably a comment
-      continue;
+    // https://stackoverflow.com/questions/30558246/how-do-i-find-the-index-of-an-element-in-an-array-vector-or-slice
+    let digit = hex_chars.iter().position(|&r| r == in_char).unwrap() as u8;
+    accumulator = (accumulator << 4) | digit;
+    first_nibble = !first_nibble;
+
+    if first_nibble {
+      out_bytes.push(accumulator);
+      accumulator = 0;
     }
   }
   println!("Writing bytes: {:x?}", out_bytes);
--- src/samples/test.asm
+++ src/samples/test.asm
@@ -1,4 +1,4 @@
-hello 68 65 6C 6C 6F world 20 77 6F 72 6C 64 ! 21
+hello 68 65 6C 6C 6F world 20 77 6F 72 6C 64 (exclamation mark) 21
 0F 00 0F
 comment
 45
