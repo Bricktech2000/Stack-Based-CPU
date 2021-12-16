9eb0f05852d530e8b76eced9a7c1084e075c6779
Bricktech2000 <bricktech2000@gmail.com>
2021-12-16 08:24:46 -0500

    code cleanup

--- src/bin/asm.rs
+++ src/bin/asm.rs
@@ -44,222 +44,132 @@ fn main() {
     index += 1;
   }
   let tokens: Vec<String> = mod_string.split_whitespace().map(str::to_string).collect();
-  println!("{:?}", tokens);
 
-  // while index < in_bytes.len() {
-  //   let in_char: u8 = in_bytes[index];
-  //   // ignore whitespace and comments
-  //   if " \n\t".contains(in_char as char) {
-  //     index += 1;
-  //     continue;
-  //   }
-  //   current_tok.push(in_char as char);
-  //   if last_was_offset && current_tok.as_str() != "x" {
-  //     println!("Error: Expected argument.");
-  //     break_type = const_arg;
-  //     break;
-  //   }
+  index = 0;
+  while index < tokens.len() {
+    println!("{:?}", tokens[index]);
+    let current_token: &str = tokens[index].as_str();
 
-  //   let matched: bool = match current_tok.as_str() {
-  //     "x" | "p" => {
-  //       let mut hex_str: String = String::new();
-  //       index += 1;
-  //       hex_str.push(in_bytes[index] as char);
-  //       index += 1;
-  //       hex_str.push(in_bytes[index] as char);
-  //       let hex_num = u8::from_str_radix(hex_str.as_str(), 16).expect("Unable to parse hex.");
-  //       if current_tok.as_str() == "x" {
-  //         if last_was_offset {
-  //           last_was_offset = false;
-  //           if hex_num < 0x40 {
-  //             let instruction = out_bytes.pop().unwrap();
-  //             out_bytes.push(instruction | hex_num);
-  //           } else {
-  //             println!("Error: Invalid argument {} for instruction.", hex_num);
-  //             break_type = const_arg;
-  //           }
-  //         } else if hex_num < 0x10 {
-  //           out_bytes.push(0xF0 | hex_num);
-  //         } else {
-  //           out_bytes.push(0x01);
-  //           out_bytes.push(hex_num);
-  //         }
-  //       } else {
-  //         out_bytes.push(hex_num);
-  //       }
-  //       true },
-  //     "nop" => {
-  //       out_bytes.push(0x00);
-  //       true },
-  //     "hlt" => {
-  //       out_bytes.push(0x02);
-  //       true },
-  //     "out" => {
-  //       out_bytes.push(0x08);
-  //       true },
-  //     "iin" => {
-  //       out_bytes.push(0x09);
-  //       true },
+    match current_token {
+      "nop" => { out_bytes.push(0x00) },
+      "hlt" => { out_bytes.push(0x02) },
+      "out" => { out_bytes.push(0x08) },
+      "iin" => { out_bytes.push(0x09) },
 
-  //     "lda" => {
-  //       out_bytes.push(0x11);
-  //       true },
-  //     "sta" => {
-  //       out_bytes.push(0x12);
-  //       true },
-  //     "lds" => {
-  //       out_bytes.push(0x13);
-  //       true },
-  //     "sts" => {
-  //       out_bytes.push(0x14);
-  //       true },
-  //     "ldi" => {
-  //       out_bytes.push(0x15);
-  //       true },
-  //     "sti" => {
-  //       out_bytes.push(0x16);
-  //       true },
-  //     "ldp" => {
-  //       out_bytes.push(0x17);
-  //       true },
-  //     "stp" => {
-  //       out_bytes.push(0x18);
-  //       true },
+      "lda" => { out_bytes.push(0x11) },
+      "sta" => { out_bytes.push(0x12) },
+      "lds" => { out_bytes.push(0x13) },
+      "sts" => { out_bytes.push(0x14) },
+      "ldi" => { out_bytes.push(0x15) },
+      "sti" => { out_bytes.push(0x16) },
+      "ldp" => { out_bytes.push(0x17) },
+      "stp" => { out_bytes.push(0x18) },
 
-  //     "dup" => {
-  //       out_bytes.push(0x19);
-  //       true },
-  //     "drp" => {
-  //       out_bytes.push(0x1A);
-  //       true },
-  //     "swp" => {
-  //       out_bytes.push(0x1B);
-  //       true },
+      "dup" => { out_bytes.push(0x19) },
+      "drp" => { out_bytes.push(0x1A) },
+      "swp" => { out_bytes.push(0x1B) },
 
-  //     "add" => {
-  //       out_bytes.push(0x20);
-  //       true },
-  //     "adc" => {
-  //       out_bytes.push(0x21);
-  //       true },
-  //     "sub" => {
-  //       out_bytes.push(0x22);
-  //       true },
-  //     "sbc" => {
-  //       out_bytes.push(0x23);
-  //       true },
-  //     "inc" => {
-  //       out_bytes.push(0x24);
-  //       true },
-  //     "dec" => {
-  //       out_bytes.push(0x25);
-  //       true },
-  //     "ilt" => {
-  //       out_bytes.push(0x26);
-  //       true },
-  //     "lgt" => {
-  //       out_bytes.push(0x27);
-  //       true },
-  //     "ieq" => {
-  //       out_bytes.push(0x28);
-  //       true },
-  //     "nez" => {
-  //       out_bytes.push(0x29);
-  //       true },
-  //     "neg" => {
-  //       out_bytes.push(0x2A);
-  //       true },
-  //     "abs" => {
-  //       out_bytes.push(0x2B);
-  //       true },
+      "add" => { out_bytes.push(0x20) },
+      "adc" => { out_bytes.push(0x21) },
+      "sub" => { out_bytes.push(0x22) },
+      "sbc" => { out_bytes.push(0x23) },
+      "inc" => { out_bytes.push(0x24) },
+      "dec" => { out_bytes.push(0x25) },
+      "ilt" => { out_bytes.push(0x26) },
+      "lgt" => { out_bytes.push(0x27) },
+      "ieq" => { out_bytes.push(0x28) },
+      "nez" => { out_bytes.push(0x29) },
+      "neg" => { out_bytes.push(0x2A) },
+      "abs" => { out_bytes.push(0x2B) },
       
-  //     "not" => {
-  //       out_bytes.push(0x30);
-  //       true },
-  //     "oor" => {
-  //       out_bytes.push(0x31);
-  //       true },
-  //     "and" => {
-  //       out_bytes.push(0x32);
-  //       true },
-  //     "xor" => {
-  //       out_bytes.push(0x33);
-  //       true },
-  //     "xnd" => {
-  //       out_bytes.push(0x34);
-  //       true },
+      "not" => { out_bytes.push(0x30) },
+      "oor" => { out_bytes.push(0x31) },
+      "and" => { out_bytes.push(0x32) },
+      "xor" => { out_bytes.push(0x33) },
+      "xnd" => { out_bytes.push(0x34) },
 
-  //     "sto" => {
-  //       last_was_offset = true;
-  //       out_bytes.push(0x40);
-  //       true },
-  //     "ldo" => {
-  //       last_was_offset = true;
-  //       out_bytes.push(0x80);
-  //       true },
-  //     "skp" => {
-  //       // TODO: could overflow, see "x" | "p"
-  //       last_was_offset = true;
-  //       out_bytes.push(0xC0);
-  //       true },
-  //     //TODO: shifts
+      "sto" | "ldo" | "skp" => {
+        index += 1;
+        let op_code = match current_token {
+          "sto" => 0x40,
+          "ldo" => 0x80,
+          "skp" => 0xC0, // TODO: could overflow
+          _ => { die(0x05, current_token); 0x00 },
+        };
+        let current_token = tokens[index].as_str();
+        match get_immediate(current_token) {
+          Ok(value) => {
+            if value < 0x20 { out_bytes.push(op_code | value) }
+            else { die(0x02, current_token) }
+          },
+          Err(code) => die(code, current_token),
+        }
+      },
+      //TODO: shifts
 
-  //     "#" => {
-  //       index += 1;
-  //       while in_bytes[index] as char != '\n' { index += 1; }
-  //       true },
-  //     "$" => {
-  //       let mut label: String = String::new();
-  //       while !" \n\t".contains(in_bytes[index + 1] as char) {
-  //         index += 1;
-  //         label.push(in_bytes[index] as char);
-  //       }
-  //       out_bytes.push(0x01);
-  //       out_bytes.push(0xCC);
-  //       mention_to_label.insert(out_bytes.len() as u8 - 1, label);
-  //       true },
-  //     "lbl$" => {
-  //       let mut label: String = String::new();
-  //       while !" \n\t".contains(in_bytes[index + 1] as char) {
-  //         index += 1;
-  //         label.push(in_bytes[index] as char);
-  //       }
-  //       label_to_value.insert(label, out_bytes.len() as u8);
-  //       true },
-  //     _ => { false },
-  //   };
+      "lbl" => {
+        index += 1;
+        let current_token = tokens[index].as_str();
+        label_to_value.insert(current_token.to_string(), out_bytes.len() as u8);
+      },
+      _ if current_token.starts_with("$") => {
+        out_bytes.push(0x01);
+        out_bytes.push(0xCC);
+        mention_to_label.insert(out_bytes.len() as u8 - 1, current_token.to_string());
+      },
+      _ if current_token.starts_with("x") => {
+        match get_immediate(current_token) {
+          Ok(value) => {
+            if value < 0x10 {
+              out_bytes.push(0xF0 | value);
+            } else {
+              out_bytes.push(0x01);
+              out_bytes.push(value);
+            }
+          },
+          Err(code) => die(code, current_token),
+        }
+      },
+      _ if current_token.starts_with("p") => {
+        match get_immediate(current_token) {
+          Ok(value) => out_bytes.push(value),
+          Err(code) => die(code, current_token),
+        }
+      },
+      _ => { die(0x03, current_token) },
+    };
+    index += 1;
+  }
+  for (mention, label) in mention_to_label.iter() {
+    match label_to_value.get(label) {
+      Some(value) => out_bytes[*mention as usize] = *value,
+      None => die(0x04, label),
+    }
+  }
 
-  //   if matched { current_tok = String::new(); };
-
-  //   index += 1;
-  //   if break_type != const_ok { break; }
-  // }
-  // for (mention, label) in mention_to_label.iter() {
-  //   match label_to_value.get(label) {
-  //     Some(value) => {
-  //       out_bytes[*mention as usize] = *value
-  //     },
-  //     None => { break_type = const_lbl; println!("Label {} not found.", label) }
-  //   }
-  // }
-
-  // if break_type == const_ok && current_tok.len() > 0 { break_type = const_eof; }
-
-  // println!("Assembly complete. {}", const_break_lookup[break_type]);
-  // if break_type == const_ok {
-  //   println!("Writing bytes...");
-  //   fs::write(format!("{}{}", &args[1], ".bin"), out_bytes).expect("Unable to write file.");
-  //   println!("Done.");
-  // }
+  println!("Assembly completed succsessefully.");
+  println!("Writing bytes...");
+  fs::write(format!("{}{}", &args[1], ".bin"), out_bytes).expect("Unable to write file.");
+  println!("Done.");
 }
 
-fn die(code: usize, instruction_pointer: u8, value: u8) {
+fn get_immediate(current_token: &str) -> Result<u8, usize> {
+  let hex_str = &current_token[1..];
+  let hex_num = u8::from_str_radix(hex_str, 16).expect("Unable to parse hex.");
+  Ok(hex_num)
+}
+
+fn die(code: usize, value: &str) {
   let message: &str = [
     "Success ",
-    "",
+    "Invalid immediate: ",
+    "Immediate Overflow: ",
+    "Invalid Instruction: ",
+    "Label Not Found: ",
+    "Internal Error: Unknown opcode: ",
   ][code];
 
-  println!("Fatal Error at {:02x}: {}{:02x}.", instruction_pointer, message, value);
+  println!("Fatal Error: {}{}.", message, value);
   println!("Exiting.");
   std::process::exit(code as i32);
 }
